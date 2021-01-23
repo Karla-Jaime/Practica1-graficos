7ed8164ca9bf22adb7943df9ee21ac0a09175d6e
Karla jaime <you@example.com>
2021-01-22 21:27:50 -0700

    practica graficos

---
+++ routes/web.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return "Hola, estás en la raíz del dominio";
+});
+
+Route::get('/noticias', function () {
+    return "Hola, estás en la sección de noticias";
+});
+
+
+Route::post("/noticias", function(){
+    return "Esta ruta es para agregar nuevas noticias";
+});
+
+Route:: match(["get","post, delete"],"/blog", function()
+{
+    return "Aquí llegamos con get y post";
+});
+
+Route::any("/promociones",function()
+{
+    return"Aquí llegamos con todos los metodos HTPP";
+});
+
+Route::get("/usuarios/{id}", function($id){
+    return "Aquí se muestra la información del usuario: " . $id;
+});
+
+Route::get("/bienvenida/{nombre?}", function ($nombre = "Usuario")
+{
+    $respuesta = "Bienvenido";
+    /*if ($nombre) {*/
+        $respuesta .= " " . $nombre;
+    /*}else{
+        $respuesta .= " usuario";
+    }*/
+    return $respuesta;
+});
